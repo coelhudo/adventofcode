dbebe98c28042f8cbdc03c8e8ab9c94b285865b2
coelhudo <coelhudo@gmail.com>
2020-12-03 01:29:19 -0500

    add solution to day 3 problem (part 2)

--- src/3.cpp
+++ src/3.cpp
@@ -4,41 +4,55 @@
 #include <iterator>
 #include <vector>
 #include <algorithm>
+#include <array>
 
 
 int main(int argc, char *argv[])
 {
     assert(("expected input", argc == 2));
 
-    constexpr auto hstride = 3;
-    constexpr auto vstride = 1;
+    //strategies with first as horizontal stride and second as vertical stride
+    std::array<std::pair<std::size_t, std::size_t>, 5> strategies = {
+        std::make_pair(1, 1), {3, 1}, {5, 1}, {7, 1}, {1, 2}
+    };
+
 
     std::vector<std::string> toboggan_grid;
 
+    //read input
     std::ifstream ifs{argv[1]};
 
     std::copy(std::istream_iterator<std::string>{ifs}, std::istream_iterator<std::string>{},
               std::back_inserter(toboggan_grid));
 
-    std::size_t current_hpos = hstride;
-    std::size_t current_vpos = vstride;
+    int trees_counter = 1;
 
-    const auto max_unique_length = toboggan_grid.at(0).size();
+    for(auto strategy : strategies )
+    {
+        auto [hstride, vstride] = strategy;
+        auto current_hpos = hstride;
+        auto current_vpos = vstride;
 
-    int tree_counter = 0;
+        const auto max_unique_length = toboggan_grid.at(0).size();
 
-    while(current_vpos != toboggan_grid.size()) {
+        int tree_counter = 0;
 
-        char tile = toboggan_grid.at(current_vpos).at(current_hpos);
+        while(current_vpos < toboggan_grid.size()) {
 
-        current_hpos = (current_hpos + hstride) % max_unique_length;
-        current_vpos += vstride;
+            char tile = toboggan_grid.at(current_vpos).at(current_hpos);
 
-        if(tile == '#')
-            ++tree_counter;
+            current_hpos = (current_hpos + hstride) % max_unique_length;
+            current_vpos += vstride;
+
+            if(tile == '#')
+                ++tree_counter;
+        }
+        std::cout << "Right " << hstride << ", down " << vstride << ". Tree encounters: " << tree_counter << '\n';
+        trees_counter *= tree_counter;
     }
 
-    std::cout << "Trees: " << tree_counter << '\n';
+
+    std::cout << "Trees multiplied: " << trees_counter << '\n';
 
 
     return 0;
