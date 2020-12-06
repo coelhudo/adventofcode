1bd3864b8b44cbb707988230ebd5f78597c5624b
coelhudo <coelhudo@gmail.com>
2020-12-06 01:16:15 -0500

    add solution to day 6 problem (part 2)

--- src/6.cpp
+++ src/6.cpp
@@ -5,25 +5,25 @@
 #include <vector>
 #include <set>
 #include <numeric>
+#include <array>
+#include <algorithm>
 
-int main(int argc, char *argv[])
+std::vector<int> check_answers_a(std::ifstream &ifs)
 {
-    assert(("expect input", argc == 2));
-
-    std::ifstream ifs{argv[1]};
-
     std::string line;
     std::ostringstream group;
-    std::vector<std::string> groups;
+    std::vector<int> groups;
 
+    //O(n) where n is the number of lines
     while(!ifs.eof())
     {
         std::getline(ifs, line);
         if(line.empty())
         {
             auto group_str = group.str();
+            //O(m) where m is the number of answers per group
             std::set<char> questions{std::cbegin(group_str), std::cend(group_str)};
-            groups.emplace_back(std::cbegin(questions), std::cend(questions));
+            groups.push_back(questions.size());
             group.str("");
         }
         else
@@ -33,10 +33,66 @@ int main(int argc, char *argv[])
 
     }
 
-    std::cout << std::accumulate(std::begin(groups), std::end(groups), 0, [](int i, std::string const & questions)
+    return groups;
+}
+
+std::vector<int> check_answers_b(std::ifstream &ifs)
+{
+    std::string line;
+    std::ostringstream group;
+    std::vector<int> groups;
+    int ingroup_counter{};
+
+    //O(n) where n is the number of lines
+    while(!ifs.eof())
     {
-        return i + questions.size();
-    }) << '\n';
+        std::getline(ifs, line);
+        if(line.empty())
+        {
+            auto group_str = group.str();
+            std::array<int, 26> questions_tracker{0};
+
+
+            //O(m) where m is the number of answers per group
+            for(auto c : group_str)
+            {
+                //I'm assuming that we are using an ASCII table where all
+                //ASCII codes are sequential.
+                questions_tracker[c - 'a'] += 1;
+            }
+
+            //O(1) since questions_tracker size is 26
+            auto total = std::count(std::begin(questions_tracker), std::end(questions_tracker), ingroup_counter);
+            groups.push_back(total);
+
+            //reset group
+            group.str("");
+            ingroup_counter = 0;
+        }
+        else
+        {
+            group << line;
+            ++ingroup_counter;
+        }
+
+    }
+
+    return groups;
+}
+
+int main(int argc, char *argv[])
+{
+    assert(("expect input", argc == 2));
+
+    std::ifstream ifs{argv[1]};
+
+    //part a
+    //auto groups = check_answers_a(ifs);
+
+    //part b
+    auto groups = check_answers_b(ifs);
+
+    std::cout << std::accumulate(std::begin(groups), std::end(groups), 0) << '\n';
 
 
 
