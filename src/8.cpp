f6190709398a0fae9cd1a39a111a46a58764da34
coelhudo <coelhudo@gmail.com>
2020-12-08 02:03:14 -0500

    improve solution to day 8 problem (part 1)

--- src/8.cpp
+++ src/8.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <set>
 #include <algorithm>
+#include <tuple>
 
 enum class OpCode {JMP, NOP, ACC};
 
@@ -25,10 +26,48 @@ struct Instruction
     OpCode op_code;
 };
 
-std::ostream &operator<<(std::ostream &os, Instruction const& instruction)
+std::ostream& operator<<(std::ostream &os, Instruction const& instruction)
 {
     os << instruction.name << ' ' << instruction.value;
     return os;
+}
+
+std::tuple<int, std::size_t, bool> detect_loop(std::vector<Instruction> const& instructions)
+{
+    int counter{};
+    std::set<int> instructions_executed;
+    // assuming that no instruction will try to make sp less than 0
+    std::size_t sp = 0;
+    std::size_t sp_before = 0;
+    // O(m) where m is the number of instructions since each
+    //instructions can execute at most one time
+    while(sp != instructions.size()) {
+        if(std::find(std::cbegin(instructions_executed),
+                     std::cend(instructions_executed), sp) != std::cend(instructions_executed))
+            return std::make_tuple(counter, sp_before, true);
+
+        instructions_executed.insert(sp);
+        auto instruction = instructions.at(sp);
+
+        //std::cout << "Executing " << instruction << '\n';
+        //std::cout << sp << " " << sp_before << "\n";
+        sp_before = sp;
+        switch(instruction.op_code)
+            {
+            case OpCode::JMP:
+                sp += instruction.value;
+                break;
+            case OpCode::ACC:
+                counter += instruction.value;
+            case OpCode::NOP:
+                ++sp;
+                break;
+            default:
+                std::cerr << "this is not suppose to happen\n";
+            }
+    }
+
+    return std::make_tuple(counter, sp_before, false);
 }
 
 int main(int argc, char *argv[])
@@ -52,38 +91,14 @@ int main(int argc, char *argv[])
             instructions.emplace_back(name, value);
         }
 
-    //execute instructions
-    int counter = 0;
-    int sp = 0;
+    //part a
+    auto result = detect_loop(instructions);
+    std::cout << "Counter before loop: " << std::get<0>(result) << "" << '\n';
+    if(std::get<2>(result))
+        std::cout << "Loop found at " << std::get<1>(result) << '\n';
+    else
+        std::cout << "No loop found\n";
 
-    std::set<int> instructions_executed;
-
-    while(true) {
-        if(std::find(std::cbegin(instructions_executed),
-                     std::cend(instructions_executed), sp) != std::cend(instructions_executed))
-            break;
-
-        instructions_executed.insert(sp);
-        auto instruction = instructions.at(sp);
-
-        //std::cout << "Executing " << instruction << '\n';
-
-        switch(instruction.op_code)
-            {
-            case OpCode::JMP:
-                sp += instruction.value;
-                break;
-            case OpCode::ACC:
-                counter += instruction.value;
-            case OpCode::NOP:
-                ++sp;
-                break;
-            default:
-                std::cerr << "this is not suppose to happen\n";
-            }
-    }
-
-    std::cout << "Counter before loop: " << counter << '\n';
 
     return 0;
 }
