067e053543dc749d182a989583fa9a32ddadb85e
coelhudo <coelhudo@gmail.com>
2020-12-15 17:24:29 -0500

    add solution to day 15 problem (part 2)

--- src/15.cpp
+++ src/15.cpp
@@ -2,29 +2,38 @@
 #include <iterator>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
 
 int main(int argc, char *argv[])
 {
-    std::vector<int> input{15,12,0,14,3,1};
-    const auto initial_size = input.size();
+    //I have to keep just the last position of each number I only need something associative
+    //Since the order doesn't matter, an unordered_map yields a better performance
+    std::unordered_map<int,int> input{{15,0}, {12,1}, {0,2}, {14, 3}, {3,4}, {1,5}};
+    auto nth_number{30000000 - input.size()};
+    auto current = 0;
+    auto current_pos = 6;
 
-    auto current = input.rbegin();
-    auto nth_number{2020 - input.size()};
+    int last_n{};
+
     while(nth_number--) {
-        auto pos = std::find(std::next(current), std::rend(input), *current);
-        if(pos != std::rend(input)) {
-            input.push_back(pos - current);
+        last_n = current;
+
+        auto pos = input.find(current);
+        bool has_been_spoken = pos != std::end(input);
+
+        if(has_been_spoken) {
+            auto last_time_spoken = pos->second;
+            input[current] = current_pos;
+            current = current_pos - last_time_spoken;
+            ++current_pos;
         } else {
-            input.push_back(0);
+            input[current] = current_pos;
+            ++current_pos;
+            current = 0;
         }
-        current = input.rbegin();
     }
 
-    for(auto n: input)
-        std::cout << n << ' ';
-    std::cout << '\n';
-
-    std::cout << *input.rbegin() << '\n';
+    std::cout << last_n << '\n';
 
     return 0;
 }
