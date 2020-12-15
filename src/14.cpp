956621b65130fd1a0b1f91e87a0d92452347ed75
coelhudo <coelhudo@gmail.com>
2020-12-14 21:20:23 -0500

    refactor solution to day 14

--- src/14.cpp
+++ src/14.cpp
@@ -4,52 +4,41 @@
 #include <cassert>
 #include <string>
 #include <unordered_map>
-#include <vector>
 #include <bitset>
 #include <algorithm>
 #include <regex>
+#include <vector>
+#include <iterator>
 
-struct MaskEntry {
-    std::string bitmask_and;
-    std::string bitmask_or;
-};
+constexpr std::size_t BITS = 36;
 
-int main(int argc, char *argv[])
+std::size_t part_a(std::vector<std::string> const& entries)
 {
-    assert(("expect input", argc == 2));
-
-    std::ifstream ifs{argv[1]};
-
-    std::string line;
-
-    constexpr std::size_t bits = 36;
-    std::bitset<bits> bitmask_and;
-    std::bitset<bits> bitmask_or;
+    std::bitset<BITS> bitmask_and;
+    std::bitset<BITS> bitmask_or;
 
     std::map<std::size_t, std::size_t> memory;
 
-
-
-    while(std::getline(ifs, line))
+    for(auto entry : entries)
     {
 
-        if(line.substr(0, 4) == "mask") {
-            const std::string bitmask = line.substr(7);
+        if(entry.substr(0, 4) == "mask") {
+            const std::string bitmask = entry.substr(7);
             std::string bitmask_and_value = bitmask;
             std::string bitmask_or_value = bitmask;
             std::replace(std::begin(bitmask_and_value), std::end(bitmask_and_value), 'X', '1');
             std::replace(std::begin(bitmask_or_value), std::end(bitmask_or_value), 'X', '0');
 
-            bitmask_and = std::bitset<bits>(bitmask_and_value);
-            bitmask_or = std::bitset<bits>(bitmask_or_value);
+            bitmask_and = std::bitset<BITS>(bitmask_and_value);
+            bitmask_or = std::bitset<BITS>(bitmask_or_value);
         } else {
             std::smatch match;
             std::regex pattern{R"(mem\[(\d+)\] = (\d+))"};
-            std::regex_match(line, match, pattern);
+            std::regex_match(entry, match, pattern);
             std::size_t address = std::stoull(match[1]);
             std::size_t value = std::stoull(match[2]);
 
-            std::bitset<bits> valuebits(value);
+            std::bitset<BITS> valuebits(value);
             auto result = valuebits & bitmask_and | bitmask_or;
             memory[address] = result.to_ullong();
         }
@@ -62,6 +51,24 @@ int main(int argc, char *argv[])
         //std::cout << address << ' ' << value << '\n';
     }
 
+    return total;
+}
+
+int main(int argc, char *argv[])
+{
+    assert(("expect input", argc == 2));
+
+    std::ifstream ifs{argv[1]};
+
+    std::vector<std::string> entries;
+
+    std::string line;
+    while(std::getline(ifs, line))
+        entries.push_back(line);
+
+
+    //Part A
+    auto total = part_a(entries);
     std::cout << "total: " << total << '\n';
 
     return 0;
