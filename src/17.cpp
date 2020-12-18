28e4c4f367ee808e50251542482426575018c219
coelhudo <coelhudo@gmail.com>
2020-12-18 02:57:24 -0500

    add solution to day 17 problem (part 2)

--- src/17.cpp
+++ src/17.cpp
@@ -7,26 +7,42 @@
 #include <tuple>
 #include <vector>
 #include <functional>
+#include <iterator>
 
 typedef std::tuple<int,int,int> Coordinate;
+typedef std::tuple<int,int,int,int> HyperCoordinate;
 
 std::ostream & operator<<(std::ostream &os, Coordinate const& coord){
     os << std::get<0>(coord) << ' ' << std::get<1>(coord) << ' ' << std::get<2>(coord);
     return os;
 }
 
+std::ostream & operator<<(std::ostream &os, HyperCoordinate const& coord){
+    os << std::get<0>(coord) << ' ' << std::get<1>(coord) << ' ' << std::get<2>(coord) << ' ' << std::get<3>(coord);
+    return os;
+}
+
 Coordinate operator+(Coordinate lhs, Coordinate rhs) {
     return {std::get<0>(lhs) + std::get<0>(rhs),
-            std::get<1>(lhs) + std::get<1>(rhs),
-            std::get<2>(lhs) + std::get<2>(rhs)};
+        std::get<1>(lhs) + std::get<1>(rhs),
+        std::get<2>(lhs) + std::get<2>(rhs)};
+}
+
+HyperCoordinate operator+(HyperCoordinate lhs, HyperCoordinate rhs) {
+    return {std::get<0>(lhs) + std::get<0>(rhs),
+        std::get<1>(lhs) + std::get<1>(rhs),
+        std::get<2>(lhs) + std::get<2>(rhs),
+        std::get<3>(lhs) + std::get<3>(rhs)};
 }
 
 typedef std::set<Coordinate> Cubes3DSpace;
+typedef std::set<HyperCoordinate> Cubes4DSpace;
 
-void apply_rule(Cubes3DSpace const& space, Cubes3DSpace const &neighbours_delta,
-                Cubes3DSpace const& reference_cubes_space, Cubes3DSpace &new_cubes_space,
+template<typename CubesSpaceT>
+void apply_rule(CubesSpaceT const& space, CubesSpaceT const &neighbours_delta,
+                CubesSpaceT const& reference_cubes_space, CubesSpaceT &new_cubes_space,
                 std::function<bool(int)> rule,
-                std::function<void(Coordinate)> process_inactive_neighbour = [](Coordinate neighbour){}) {
+                std::function<void(typename CubesSpaceT::value_type)> process_inactive_neighbour = [](typename CubesSpaceT::value_type neighbour){}) {
     for(auto cube: space)
     {
         int count{};
@@ -44,66 +60,90 @@ void apply_rule(Cubes3DSpace const& space, Cubes3DSpace const &neighbours_delta,
     }
 }
 
+template<typename CubesSpaceT>
+void execute(CubesSpaceT &cubes_space, CubesSpaceT const& neighbours_delta ) {
+    int cycles = 6;
+    while(cycles--)
+    {
+        CubesSpaceT inactive_cubes_with_neighbours;
+        CubesSpaceT new_cubes_space;
+
+        //Apply first rule on active cubes
+        apply_rule<CubesSpaceT>(cubes_space, neighbours_delta, cubes_space, new_cubes_space,
+                                             [](int count) { return count == 2 || count == 3;},
+                                             [&inactive_cubes_with_neighbours](typename CubesSpaceT::value_type coordinate) {
+                                                 inactive_cubes_with_neighbours.emplace(coordinate);
+                                             });
+
+
+        //Apply second rule on inactive cubes
+        apply_rule<CubesSpaceT>(inactive_cubes_with_neighbours, neighbours_delta, cubes_space, new_cubes_space,
+                                             [](int count) { return count == 3;});
+
+        cubes_space = new_cubes_space;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     assert(("expect input", argc == 2));
 
     std::ifstream ifs{argv[1]};
+    std::vector<std::string> input{std::istream_iterator<std::string>{ifs}, std::istream_iterator<std::string>{}};
 
-    Cubes3DSpace cubes_space;
-
-    std::string line;
-    int current_x = 0;
-    while(ifs >> line) {
-        for(int current_y = 0; current_y < line.size(); ++current_y) {
-            if(line[current_y] == '#') {
-                cubes_space.emplace(0, current_x, current_y);
-            }
-        }
-        ++current_x;
-    }
-
-    Cubes3DSpace neighbours_delta;
-    Cubes3DSpace initial_deltas;
-    for(auto x :{0,1})
-        for(auto y :{0,1})
-            for(auto z: {0,1})
-                initial_deltas.emplace(x,y,z);
-
-    for(auto [x,y,z]: initial_deltas) {
-        neighbours_delta.emplace(x,y,z);
-        neighbours_delta.emplace(x*-1, y, z);
-        neighbours_delta.emplace(x, y*-1, z);
-        neighbours_delta.emplace(x, y, z*-1);
-        neighbours_delta.emplace(x*-1, y*-1, z);
-        neighbours_delta.emplace(x, y*-1, z*-1);
-        neighbours_delta.emplace(x*-1, y, z*-1);
-        neighbours_delta.emplace(x*-1, y*-1, z*-1);
-    }
-
-    int cycles = 6;
-    while(cycles--)
+    //Part A
     {
-        Cubes3DSpace inactive_cubes_with_neighbours;
-        Cubes3DSpace new_cubes_space;
+        Cubes3DSpace cubes_space;
 
-        //Apply first rule on active cubes
-        apply_rule(cubes_space, neighbours_delta, cubes_space, new_cubes_space,
-                   [](int count) { return count == 2 || count == 3;},
-                   [&inactive_cubes_with_neighbours](Coordinate coordinate) {
-                       inactive_cubes_with_neighbours.emplace(coordinate);
-                   });
+        int current_x = 0;
+        for(auto entry : input) {
+            for(int current_y = 0; current_y < entry.size(); ++current_y) {
+                if(entry[current_y] == '#') {
+                    cubes_space.emplace(0, current_x, current_y);
+                }
+            }
+            ++current_x;
+        }
 
+        Cubes3DSpace neighbours_delta;
+        for(auto x :{-1,0,1})
+            for(auto y :{-1,0,1})
+                for(auto z: {-1,0,1})
+                    neighbours_delta.emplace(x,y,z);
 
-        //Apply second rule on inactive cubes
-        apply_rule(inactive_cubes_with_neighbours, neighbours_delta, cubes_space, new_cubes_space,
-                   [](int count) { return count == 3;});
+        execute<Cubes3DSpace>(cubes_space, neighbours_delta);
 
-        cubes_space = new_cubes_space;
+        std::cout << "Part A\n";
+        std::cout << cubes_space.size() << '\n';
     }
 
-    std::cout << "Part A\n";
-    std::cout << cubes_space.size() << '\n';
+    //Part B
+    {
+        Cubes4DSpace cubes_hyperspace;
+
+        int current_x = 0;
+        for(auto entry : input) {
+            for(int current_y = 0; current_y < entry.size(); ++current_y) {
+                if(entry[current_y] == '#') {
+                    cubes_hyperspace.emplace(0, 0, current_x, current_y);
+                }
+            }
+            ++current_x;
+        }
+
+        Cubes4DSpace neighbours_delta;
+
+        for(auto x :{-1,0,1})
+            for(auto y :{-1,0,1})
+                for(auto z: {-1,0,1})
+                    for(auto w: {-1,0,1})
+                        neighbours_delta.emplace(x,y,z,w);
+
+        execute<Cubes4DSpace>(cubes_hyperspace, neighbours_delta);
+
+        std::cout << "Part B\n";
+        std::cout << cubes_hyperspace.size() << '\n';
+    }
 
     return 0;
 }
