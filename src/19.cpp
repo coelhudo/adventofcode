67264a6f405780c4adfea1b531fbf7ba84869370
coelhudo <coelhudo@gmail.com>
2020-12-28 23:29:23 -0500

    add a more clever solution to day 19 problem (part 1)
    it expands the leftmost rules. compares the leftmost rule with the
    leftmost entry rule. If both are terminal and matches, keep expanding
    and searching the rest of the entry.
    
    if there are rules and entry is empty or if there are no rules and
    entry isn't empty it means that is not a match. Otherwise it is since
    both were consumed

--- src/19.cpp
+++ src/19.cpp
@@ -5,51 +5,27 @@
 #include <cassert>
 #include <map>
 #include <vector>
+#include <list>
 #include <algorithm>
 #include <sstream>
 #include <unordered_set>
 #include <unordered_map>
 
-void f(std::unordered_multimap<std::string, std::string> const& rules,
-       std::map<std::string, std::string> const& terminal_rules,
-       std::string candidate, bool& found,
-       std::size_t maximum_nonterminal_length)
+template<typename Container>
+void print_container(Container const& subrules, char end = '\n')
 {
-    auto [start_it, end_it] = rules.equal_range(candidate);
-    if(start_it != end_it && start_it->second == "0")
-    {
-        found = true;
-        return;
-    }
-
-    for(int start = 0; start != (candidate.size() - maximum_nonterminal_length); ++start)
-    {
-        auto prefix = candidate.substr(0, start);
-        for(int n_characters = 1; n_characters <= maximum_nonterminal_length; ++n_characters)
-        {
-            auto sub = candidate.substr(start, n_characters);
-            auto [start_it, end_it] = rules.equal_range(sub);
-            while(start_it != end_it)
-            {
-                std::ostringstream oss;
-                oss << prefix;
-                oss << start_it->second;
-                oss << candidate.substr(start+n_characters);
-                f(rules, terminal_rules, oss.str(), found, maximum_nonterminal_length);
-                if(found) //don't need to keep searching
-                    return;
-                ++start_it;
-            }
-        }
-    }
+    for(auto i: subrules)
+        std::cout << i << " ";
+    std::cout << end;
 };
 
 int main(int argc, char *argv[])
 {
     assert(("expect input", argc == 2));
 
-    std::unordered_multimap<std::string, std::string> rules;
-    std::map<std::string, std::string> terminal_rules;
+    std::multimap<int, std::vector<int>> rules;
+    std::multimap<std::vector<int>, int> inverted_rules;
+    std::map<char, int> terminal_rules;
     std::vector<std::string> entries;
 
     std::ifstream ifs{argv[1]};
@@ -57,17 +33,6 @@ int main(int argc, char *argv[])
 
     bool is_a_rule = true;
 
-    auto transform_into_string = [](std::string nonterminal_symbol){
-        auto space_pos = nonterminal_symbol.find(' ');
-        while(space_pos != std::string::npos)
-        {
-            nonterminal_symbol.erase(space_pos,1);
-            space_pos = nonterminal_symbol.find(' ');
-        }
-        return nonterminal_symbol;
-    };
-
-    std::size_t maximum_nonterminal_length{0};
 
     while(std::getline(ifs, line))
     {
@@ -79,51 +44,159 @@ int main(int argc, char *argv[])
 
         if(is_a_rule) {
             auto pos = line.find(':');
-            const auto rule_number = line.substr(0, pos);
-            auto or_pos = line.find('|');
-            if(or_pos == std::string::npos) //without the | we can have a terminal or nonterminal symbol
+            const auto rule_number = std::stoi(line.substr(0, pos));
+            const auto start_quote = line.find('"');
+            if(start_quote != std::string::npos)
             {
-                const auto start_quote = line.find('"');
-                if(start_quote != std::string::npos)
-                {
-                    const auto end_quote = line.rfind('"');
-                    auto terminal_symbol = line.substr(start_quote+1, end_quote-start_quote-1);
-                    terminal_rules.insert({terminal_symbol, rule_number});
-                }
-                else
-                {
-                    auto nonterminal_symbol = transform_into_string(line.substr(pos+1));
-                    rules.insert({nonterminal_symbol, rule_number});
-                }
+                const auto end_quote = line.rfind('"');
+                auto terminal_symbol = line.substr(start_quote+1, end_quote-start_quote-1);
+                terminal_rules.insert({terminal_symbol[0], rule_number});
             }
             else
             {
-                auto first_nonterminal_symbol = transform_into_string(line.substr(pos+1, or_pos-pos-1));
-                auto second_nonterminal_symbol = transform_into_string(line.substr(or_pos+1));
-                maximum_nonterminal_length = std::max(maximum_nonterminal_length, first_nonterminal_symbol.size());
-                maximum_nonterminal_length = std::max(maximum_nonterminal_length, second_nonterminal_symbol.size());
-                rules.insert({first_nonterminal_symbol, rule_number});
-                rules.insert({second_nonterminal_symbol, rule_number});
+                std::istringstream iss{line.substr(pos+2)};
+                std::vector<int> subrules;
+
+                std::string value;
+                while(iss >> value)
+                {
+                    if(value != "|")
+                        subrules.push_back(std::stoi(value));
+
+                    if(value == "|")
+                    {
+                        rules.insert({rule_number, subrules});
+                        subrules.clear();
+                    }
+                }
+                rules.insert({rule_number, subrules});
             }
+
         } else {
             entries.push_back(line);
         }
     }
 
-    for(auto entry: entries)
+    for(auto [rule_number, subrules]: rules)
     {
-        std::ostringstream oss;
-        for(auto c: entry)
-        {
-            oss << terminal_rules.at(std::string(1,c));
-        }
+        if(subrules.size() != 1)
+            continue;
 
-        auto candidate = oss.str();
-        bool found = false;
-        f(rules, terminal_rules, candidate, found, maximum_nonterminal_length);
-        std::cout << entry << " found: " << std::boolalpha << found << '\n';
+        for(auto &entry: rules)
+        {
+            auto &rules = entry.second;
+            if(rules.at(0) == rule_number)
+                rules[0] = subrules.at(0);
+            if(rules.size() != 1 && rules.at(1) == rule_number)
+                rules[1] = subrules.at(0);
+        }
     }
 
+    for(auto [rule_number, subrules]: rules)
+    {
+        if(subrules.size() == 1)
+            continue;
+
+        inverted_rules.insert({subrules, rule_number});
+    }
+
+    // std::cout << "=============" << "\n";
+
+    // for(auto [rule_number, subrules]: rules)
+    // {
+    //     std::cout << rule_number << ": ";
+    //     for(auto subrule: subrules)
+    //         std::cout << subrule << ' ';
+    //     std::cout << "\n";
+
+    // }
+
+    // for(auto [subrules, rule_number]: inverted_rules)
+    // {
+    //     for(auto subrule: subrules)
+    //         std::cout << subrule << ' ';
+    //     std::cout << ": " << rule_number << "\n";
+    // }
+
+    auto translate_to_terminal_rules = [&terminal_rules](std::string const& entry)
+    {
+        std::list<int> result(entry.size());
+        std::transform(entry.begin(), entry.end(), result.begin(), [&terminal_rules](char c){
+            return terminal_rules[c];
+        });
+
+        return result;
+    };
+
+    int count{0};
+    for(auto entry : entries)
+    {
+        auto entry_rules = translate_to_terminal_rules(entry);
+
+        std::list<std::list<int>> expanded_rules{{0}};
+        while(!entry_rules.empty() && expanded_rules.size())
+        {
+
+            std::list<std::list<int>> expanded_rules2;
+            //expand leftmost rules
+            for(auto &subrules : expanded_rules)
+            {
+                auto leftmost_rule = subrules.front();
+
+                if (rules.find(leftmost_rule) == std::end(rules)) {
+                    expanded_rules2.push_back(subrules);
+                }
+                else
+                {
+                    subrules.pop_front();
+                    auto [first, last] = rules.equal_range(leftmost_rule);
+                    for(; first != last; ++first)
+                    {
+                        auto new_subrules = subrules;
+                        auto s = first->second;
+                        for(auto r = s.rbegin(); r != s.rend(); ++r)
+                            new_subrules.push_front(*r);
+                        expanded_rules2.push_back(new_subrules);
+                    }
+                }
+            }
+
+            expanded_rules = expanded_rules2;
+
+            const bool all_leftmost_rules_are_terminal = std::all_of(std::begin(expanded_rules),
+                                                                     std::end(expanded_rules),
+                                                                     [&rules](auto const& subrules)
+                                                                     {
+                                                                         return rules.find(subrules.front()) == std::end(rules);
+                                                                     });
+
+            if(all_leftmost_rules_are_terminal)
+            {
+                auto it = std::remove_if(std::begin(expanded_rules), std::end(expanded_rules), [&entry_rules](auto &subrules)
+                {
+                    auto leftmost_entry = entry_rules.front();
+                    auto leftmost_rule = subrules.front();
+                    bool match = leftmost_entry == leftmost_rule;
+                    if(match)
+                        subrules.pop_front();
+                    return !match;
+                });
+                expanded_rules.erase(it, expanded_rules.end());
+                if(!expanded_rules.empty())
+                {
+                    entry_rules.pop_front();
+                }
+            }
+
+            if(expanded_rules.size() == 1 && (*std::begin(expanded_rules)).empty())
+                break;
+        }
+
+        if(entry_rules.empty() && expanded_rules.size() == 1)
+            ++count;
+    }
+
+    std::cout << count << "\n";
 
 
     return 0;
