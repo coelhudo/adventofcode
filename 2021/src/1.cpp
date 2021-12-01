fc199f76ed5a74a917a59a2b868ba4079b9b5633
coelhudo <coelhudo@gmail.com>
2021-12-01 18:59:33 -0500

    add part2 of problem 1; include analysis

--- 2021/src/1.cpp
+++ 2021/src/1.cpp
@@ -4,6 +4,17 @@
 #include <vector>
 
 
+// O(n)
+auto count_increases(const std::vector<int> &inputs) {
+    auto counter = 0;
+    for(std::size_t i = 1; i < inputs.size(); ++i) {
+        if (inputs.at(i) > inputs.at(i-1)) {
+            ++counter;
+        }
+    }
+    return counter;
+}
+
 int main(int argc, char** argv) {
     if (argc == 1) {
         std::cout << "missing input file\n";
@@ -18,14 +29,18 @@ int main(int argc, char** argv) {
 
     // Part 1
     {
-        auto counter = 0;
-        for(std::size_t i = 1; i < inputs.size(); ++i) {
-            if (inputs.at(i) > inputs.at(i-1)) {
-                ++counter;
-            }
+        std::cout << count_increases(inputs) << '\n';
+    }
+
+    // Part 2
+    {
+        //O(n)
+        std::vector<int> grouped_inputs;
+        for(std::size_t i = 0; i < inputs.size() - 2; ++i) {
+            grouped_inputs.push_back(inputs.at(i) + inputs.at(i+1) + inputs.at(i+2));
         }
 
-        std::cout << counter << '\n';
+        std::cout << count_increases(grouped_inputs) << '\n';
     }
 
     return 0;
