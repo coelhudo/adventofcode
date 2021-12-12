55651786f94290baf9b06de206a04003e98b69bc
coelhudo <coelhudo@gmail.com>
2021-12-12 03:00:26 -0500

    update data structure for day 5

--- 2021/src/5.cpp
+++ 2021/src/5.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <utility>
 #include <vector>
 #include <algorithm>
 #include <sstream>
@@ -7,11 +8,8 @@
 #include <tuple>
 
 auto print_coordinates = [](const auto &span) {
-    for(auto s : span) {
-        auto first = std::get<0>(s);
-        auto second = std::get<1>(s);
-        std::cout << "(" << std::get<0>(first) << ", " << std::get<1>(first) << ") : (";
-        std::cout << std::get<0>(second) << ", " << std::get<1>(second) << ")\n";
+    for (const auto &p : span) {
+        std::cout << p.first << " => " << std::get<0>(p.second) << " " << std::get<1>(p.second) << '\n';
     }
 };
 
@@ -23,9 +21,10 @@ int main(int argc, char** argv) {
 
     std::ifstream ifs{argv[1]};
     std::string line;
-    using start = std::tuple<int, int>;
-    using end = std::tuple<int, int>;
-    std::vector<std::tuple<start, end>> vents;
+    using coordinate = std::tuple<int, int>;
+    std::multimap<int, coordinate> horizontal;
+    std::multimap<int, coordinate> vertical;
+    std::multimap<int, coordinate> diagonal;
 
     auto to_int = [](auto regex_it) {
         return std::atoi(regex_it->str().c_str());
@@ -34,13 +33,37 @@ int main(int argc, char** argv) {
     while(std::getline(ifs, line)) {
         std::regex number_regex("(\\d+)");
         auto first = std::sregex_iterator(line.begin(), line.end(), number_regex);
-        auto second = std::next(first);
-        auto third = std::next(second);
-        auto fourth = std::next(third);
-        vents.push_back({{to_int(first), to_int(second)}, {to_int(third), to_int(fourth)}});
+        auto second = std::next(first), third = std::next(second), fourth = std::next(third);
+        auto x1 = to_int(first), y1 = to_int(second), x2 = to_int(third), y2 = to_int(fourth);
+        if (x1 == x2) {
+            if(y1 <= y2)
+                vertical.emplace(std::make_pair(x1, std::make_tuple(y1, y2)));
+            else
+                vertical.emplace(std::make_pair(x1, std::make_tuple(y2, y1)));
+        } else if (y1 == y2) {
+            if(x1 <= x2)
+                horizontal.emplace(std::make_pair(y1, std::make_tuple(x1, x2)));
+            else
+                horizontal.emplace(std::make_pair(y1, std::make_tuple(x2, x1)));
+        } else {
+            // TODO
+            diagonal.emplace(std::make_pair(x1, std::make_tuple(y1, x2)));
+        }
     }
 
-    print_coordinates(vents);
+    std::cout << "horizontal\n";
+    print_coordinates(horizontal);
+    std::cout << "vertical\n";
+    print_coordinates(vertical);
+    std::cout << "diagonal\n";
+    print_coordinates(diagonal);
+    // for(auto vent : vents) {
+    //     auto first = std::get<0>(vent);
+    //     auto second = std::get<1>(vent);
+
+    //     std::cout << "(" << std::get<0>(first) << ", " << std::get<1>(first) << ") : (";
+    //     std::cout << std::get<0>(second) << ", " << std::get<1>(second) << ")\n";
+    // }
 
     return 0;
 }
