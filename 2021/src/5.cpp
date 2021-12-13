990887a2b054df2480ff41ed6de617d5e4f573f1
coelhudo <coelhudo@gmail.com>
2021-12-12 23:47:26 -0500

    add day 5 part 1

--- 2021/src/5.cpp
+++ 2021/src/5.cpp
@@ -8,12 +8,6 @@
 #include <tuple>
 #include <set>
 
-auto print_coordinates = [](const auto &span) {
-    for (const auto &p : span) {
-        std::cout << p.first << " => " << std::get<0>(p.second) << " " << std::get<1>(p.second) << '\n';
-    }
-};
-
 int main(int argc, char** argv) {
     if (argc == 1) {
         std::cout << "missing input file\n";
@@ -22,67 +16,59 @@ int main(int argc, char** argv) {
 
     std::ifstream ifs{argv[1]};
     std::string line;
-    using coordinate = std::tuple<int, int>;
-    std::multimap<int, coordinate> horizontal;
-    std::multimap<int, coordinate> vertical;
-    std::multimap<int, coordinate> diagonal;
-    std::set<int> h_entry;
-    std::set<int> v_entry;
-    std::set<int> d_entry;
-
+    std::set<std::tuple<int, int>> coordinates;
+    std::map<std::tuple<int, int>, int> overlapping_coordinates;
     auto to_int = [](auto regex_it) {
         return std::atoi(regex_it->str().c_str());
     };
 
+    int max_x = 0;
+    int max_y = 0;
     while(std::getline(ifs, line)) {
         std::regex number_regex("(\\d+)");
         auto first = std::sregex_iterator(line.begin(), line.end(), number_regex);
         auto second = std::next(first), third = std::next(second), fourth = std::next(third);
         auto x1 = to_int(first), y1 = to_int(second), x2 = to_int(third), y2 = to_int(fourth);
+
+        max_x = std::max(std::max(x1, x2), max_x);
+        max_y = std::max(std::max(y1, y2), max_y);
+
         if (x1 == x2) {
-            v_entry.insert(x1);
-            if(y1 <= y2)
-                vertical.emplace(std::make_pair(x1, std::make_tuple(y1, y2)));
-            else
-                vertical.emplace(std::make_pair(x1, std::make_tuple(y2, y1)));
+            auto start = y1, end = y2;
+            if (y1 > y2)
+                std::swap(start, end);
+
+            for (auto i = start; i <= end; ++i) {
+                auto it = coordinates.emplace(x1, i);
+                if(!it.second) {
+                    overlapping_coordinates[{x1, i}] += 1;
+                }
+            }
         } else if (y1 == y2) {
-            h_entry.insert(y1);
-            if(x1 <= x2)
-                horizontal.emplace(std::make_pair(y1, std::make_tuple(x1, x2)));
-            else
-                horizontal.emplace(std::make_pair(y1, std::make_tuple(x2, x1)));
+            auto start = x1, end = x2;
+            if (x1 > x2)
+                std::swap(start, end);
+
+            for (auto i = start; i <= end; ++i) {
+                auto it = coordinates.emplace(i, y1);
+                if(!it.second) {
+                    overlapping_coordinates[{i, y1}] += 1;
+                }
+            }
         } else {
             // TODO
-            d_entry.insert(x1);
-            diagonal.emplace(std::make_pair(x1, std::make_tuple(y1, x2)));
+            // coordinates.emplace(std::make_pair(x1, std::make_tuple(y1, x2)));
         }
     }
 
-    // std::cout << "horizontal\n";
-    // print_coordinates(horizontal);
-    // std::cout << "vertical\n";
-    // print_coordinates(vertical);
-    // std::cout << "diagonal\n";
-    // print_coordinates(diagonal);
-
-    //TODO: fix this
-    for(auto line : h_entry) {
-        auto result = horizontal.equal_range(line);
-        auto n_elements = std::distance(result.first, result.second);
-        if (n_elements > 1) {
-            auto first = result.first->second;
-            auto second = std::next(result.first)->second;
-            auto x1 = std::get<0>(first);
-            auto x2 = std::get<1>(first);
-            auto x3 = std::get<0>(second);
-            auto x4 = std::get<1>(second);
-
-            while(result.first != result.second) {
-                std::cout << result.first->first << ": " << std::get<0>(result.first->second) << ' ' << std::get<1>(result.first->second) << '\n';
-                ++result.first;
-            }
-        }
-    }
+    // for(int i = 0; i <= max_y; ++i) {
+    //     for(int j = 0; j <= max_x; ++j) {
+    //         int n_overlaps = (overlapping_coordinates.contains({j, i})) ? overlapping_coordinates[{j, i}] : 0;
+    //         std::cout << (coordinates.contains({j, i}) ? 1 + n_overlaps : 0) << ' ';
+    //     }
+    //     std::cout << '\n';
+    // }
+    std::cout << "Part 1: " << overlapping_coordinates.size() << '\n';
 
     return 0;
 }
